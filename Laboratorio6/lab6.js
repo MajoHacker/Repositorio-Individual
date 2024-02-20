ba0d625b8c9b67fce9cfa9025772a36faaff9f36
Maria Jose Gaytan Gil <mariajosegaytangil@MacBook-Air-de-Maria-104.local>
2024-02-19 21:28:09 -0600

    arreglo lab6

--- Laboratorio6/lab6.js
+++ Laboratorio6/lab6.js
@@ -1,49 +1,51 @@
 document.getElementById("boton_validar").addEventListener("click", function(event) {
-    event.preventDefault();
-    validatePassword();
-  });
-  
-  function validatePassword() {
-    var password = document.getElementById("password").value;
-    var textoValidacion = document.getElementById("texto_validacion");
-    var imagen = document.getElementById("imagen");
-    var botonValidar = document.getElementById("boton_validar");
-  
-    // criterios de contraseña
-    if (password.length < 8) {
-      textoValidacion.innerHTML = "La contraseña debe tener al menos 8 caracteres";
-      imagen.src="images/triste.jpg";
-    } else if (!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
-      textoValidacion.innerHTML = "La contraseña debe contener al menos una letra mayúscula y una letra minúscula";
-      imagen.src="images/triste.jpg";
-    } else if (!/\d/.test(password)) {
-      textoValidacion.innerHTML = "La contraseña debe contener al menos un número";
-      imagen.src="images/triste.jpg";
-    } else {
-      textoValidacion.innerHTML = "La contraseña es válida";
-      imagen.src="images/messirve.jpg";
-      botonValidar.innerHTML = "Validar otra contraseña";
-      botonValidar.className = "button is-success";
-      // limpia la contraseña
-      document.getElementById("password").value = "";
-      // elimina el controlador de eventos anterior
-      botonValidar.removeEventListener("click", validatePassword);
-      // agrega un nuevo controlador de eventos para validar otra contraseña
-      botonValidar.addEventListener("click", nuevaPassword);
-    }
-  }
-  
-  // validar otra contraseña
-  const nuevaPassword = () => {
-    var botonValidar = document.getElementById("boton_validar");
-    botonValidar.innerHTML = "Validar";
-    botonValidar.className = "button is-info";
+  event.preventDefault();
+  validatePassword();
+});
+
+function validatePassword() {
+  var password = document.getElementById("password").value;
+  var textoValidacion = document.getElementById("texto_validacion");
+  var imagen = document.getElementById("imagen");
+  var botonValidar = document.getElementById("boton_validar");
+
+  // criterios de contraseña
+  if (password.length < 8) {
+    textoValidacion.innerHTML = "La contraseña debe tener al menos 8 caracteres";
+    imagen.src = "images/triste.jpg";
+  } else if (!/[a-z]/.test(password) || !/[A-Z]/.test(password)) {
+    textoValidacion.innerHTML = "La contraseña debe contener al menos una letra mayúscula y una letra minúscula";
+    imagen.src = "images/triste.jpg";
+  } else if (!/\d/.test(password)) {
+    textoValidacion.innerHTML = "La contraseña debe contener al menos un número";
+    imagen.src = "images/triste.jpg";
+  } else {
+    textoValidacion.innerHTML = "La contraseña es válida";
+    imagen.src = "images/messirve.jpg";
+    botonValidar.innerHTML = "Validar otra contraseña";
+    botonValidar.classList.remove("is-info");
+    botonValidar.classList.add("is-success");
+    // limpia la contraseña
+    document.getElementById("password").value = "";
     // elimina el controlador de eventos anterior
-    botonValidar.removeEventListener("click", nuevaPassword);
-    // agregar el controlador de eventos original
-    botonValidar.addEventListener("click", function(event) {
-      event.preventDefault();
-      validatePassword();
-    });
+    botonValidar.removeEventListener("click", validatePassword);
+    // agrega un nuevo controlador de eventos para validar otra contraseña
+    botonValidar.addEventListener("click", resetValidation);
   }
-  
+}
+
+function resetValidation() {
+  var textoValidacion = document.getElementById("texto_validacion");
+  var imagen = document.getElementById("imagen");
+  var botonValidar = document.getElementById("boton_validar");
+
+  textoValidacion.innerHTML = "";
+  imagen.src = "images/inicio.jpg";
+  botonValidar.innerHTML = "Validar";
+  botonValidar.classList.remove("is-success");
+  botonValidar.classList.add("is-info");
+  // elimina el controlador de eventos anterior
+  botonValidar.removeEventListener("click", resetValidation);
+  // agrega un nuevo controlador de eventos para validar contraseña
+  botonValidar.addEventListener("click", validatePassword);
+}

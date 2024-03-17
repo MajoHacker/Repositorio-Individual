3c39cbeb7ee39f0538a5d151383706ca882c72e4
Maria Jose Gaytan Gil <mariajosegaytangil@MacBook-Air-de-Maria-92.local>
2024-03-16 20:48:56 -0600

    rutas (depende del permiso)

--- Laboratorio19/routes/comentarios.routes.js
+++ Laboratorio19/routes/comentarios.routes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const isAuth = require('../util/is-auth')
+const canView = require('../util/can-view');
 const comentariosController = require('../controllers/comentarios.controller');
 
-router.get('/comentarios', isAuth, comentariosController.get_comentarios);
-router.post('/comentarios', isAuth, comentariosController.post_comentarios);
+router.get('/comentarios', isAuth, canView, comentariosController.get_comentarios);
+router.post('/comentarios', isAuth, canView, comentariosController.post_comentarios);
 
-router.get('/goles', isAuth, comentariosController.get_goles);
+router.get('/goles', isAuth, canView, comentariosController.get_goles);
 
-router.get('/majo', isAuth, comentariosController.get_majo);
+router.get('/majo', isAuth, canView, comentariosController.get_majo);
 
-router.get('/entrevista', isAuth, comentariosController.get_entrevista);
+router.get('/entrevista', isAuth, canView, comentariosController.get_entrevista);
 
 module.exports = router;
--- Laboratorio19/routes/fan.routes.js
+++ Laboratorio19/routes/fan.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const isAuth = require('../util/is-auth')
+const canView = require('../util/can-view');
+const canBuild = require('../util/can-registrar');
 const fanController = require('../controllers/fan.controller');
 
-router.get('/fan', isAuth, fanController.get_fans);
-router.post('/fan', isAuth, fanController.post_fans);
-router.get('/:idfans', isAuth, fanController.get_root);
+router.get('/fan', isAuth, canRegistrar, fanController.get_fans);
+router.post('/fan', isAuth, canRegistrar, fanController.post_fans);
+router.get('/:idfans', isAuth, canView, fanController.get_root);
 router.get('/', isAuth, fanController.get_root);
 
 module.exports = router;
